3b37990172d92302ce91aa9f7a168fac052296c0
endriwmsi <endriwmsi@gmail.com>
2023-06-27 17:53:21 -0300

    Created AuthorBook repository and interface

---
+++ Data/Repositories/AuthorBookRepository.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementAPI.Data.Context;
+using LibraryManagementAPI.Domain.Entities;
+using LibraryManagementAPI.Domain.Interfaces;
+
+namespace LibraryManagementAPI.Data.Repositories
+{
+    public class AuthorBookRepository : IAuthorBookRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthorBookRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void AddAuthor(Author author)
+        {
+            _context.Authors.Add(author);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Domain/Interfaces/IAuthorBookRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementAPI.Data.Context;
+using LibraryManagementAPI.Domain.Entities;
+using LibraryManagementAPI.Domain.Interfaces;
+
+namespace LibraryManagementAPI.Domain.Interfaces
+{
+    public interface IAuthorBookRepository
+    {
+        void AddAuthor(Author author);
+    }
+}
