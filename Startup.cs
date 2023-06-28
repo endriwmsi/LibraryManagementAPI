46b6dcefca3cd2e1f4c7bcb9d8004b85fad2faa6
endriwmsi <endriwmsi@gmail.com>
2023-06-28 16:45:54 -0300

    Added Swagger config to run locally

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using LibraryManagementAPI.Data.Context;
 using LibraryManagementAPI.Data.Repositories;
 using LibraryManagementAPI.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 
 
@@ -29,16 +27,24 @@ namespace LibraryManagementAPI
             // Configuração dos repositórios
             services.AddScoped<IBookRepository, BookRepository>();
             services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IAuthorBookRepository, AuthorBookRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
 
             // Configuração do AutoMapper
             services.AddAutoMapper(typeof(Program));
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Library Management API", Version = "v1" });
+            });
 
             services.AddControllers();
 
             services.AddControllers().AddJsonOptions(options =>
             {
                 options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+                options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
             });
 
         }
@@ -55,6 +61,12 @@ namespace LibraryManagementAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nome do seu projeto v1");
             });
         }
     }
